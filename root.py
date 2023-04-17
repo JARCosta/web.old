48d888e8c69324b1426c724a91919fac1df837c7
João Costa <joaoarcosta@tecnico.ulisboa.pt>
2023-04-17 13:21:08 +0100

    to test refactor homeURL so only need to change in utils

--- root.py
+++ root.py
@@ -3,7 +3,8 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 import serverTest
+import utils
 
 
 def display():
-    return render_template("root.html", title="Hellow")
+    return render_template("root.html", title="Hellow", homeURL = utils.HOME_URL)
